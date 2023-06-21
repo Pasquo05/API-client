9533f601b22cb53078c59d3a7c1e24e70fb1e151
Matteo Pasquinucci <matteopasquinucci@MacBook-Pro-di-francesco.local>
2023-06-21 16:37:53 +0200

    update (add git ignore)

---
+++ funcClientMain/funcClientMain.go
@@ -0,0 +1,69 @@
+package funcClientMain
+
+import (
+	prjclient "Matteo/http/prjAPIclient/prjClient"
+	"fmt"
+	"net/http"
+)
+
+type client struct {
+	url string
+	cl  *http.Client
+}
+
+func NewClient(urlInput string) client {
+	c := client{
+		url: urlInput,
+		cl:  &http.Client{},
+	}
+	return c
+}
+
+func (c client) DeleteBook() {
+
+	Id := ""
+	fmt.Scanln(&Id)
+	prjclient.GetRequest(fmt.Sprintf("%s/book/delete/%s", c.url, Id))
+}
+
+func NewBook() prjclient.Book {
+
+	/*
+		fmt.Println("Scrivere id , titolo , desc")
+
+		idInput := ""
+		titleInput := ""
+		descInput := ""
+
+		fmt.Scanln(&idInput)
+		fmt.Scanln(&titleInput)
+		fmt.Scanln(&idInput)
+	*/
+	value := prjclient.Book{Id: "1", Title: "ciao", Desc: "ciao"}
+
+	return value
+}
+
+func (c client) CreateBook() {
+
+	book := NewBook()
+
+	url := fmt.Sprintf("%s/book/post", c.url)
+
+	prjclient.PostRequest(url, book)
+
+}
+
+func (a client) GetBooks() {
+	url := fmt.Sprintf("%s/books", a.url)
+	prjclient.GetRequest(url)
+}
+
+func (a client) GetBook() {
+
+	Id := ""
+	fmt.Scanln(&Id)
+	url := fmt.Sprintf("%s/book/%s", a.url, Id)
+	prjclient.GetRequest(url)
+
+}
---
+++ prjClient/funcClient.go
@@ -0,0 +1,77 @@
+package funcCLient
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+type Book struct {
+	Id    string `json:"id"`
+	Title string `json:"title"`
+	Desc  string `json:"desc"`
+}
+
+func PostRequest(url string, bookInput interface{}) {
+
+	book, err := json.Marshal(bookInput)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	resp, err := http.Post(url, "application/json", bytes.NewBuffer(book))
+	if err != nil {
+		fmt.Println("Errore durante l'esecuzione della richiesta:", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Errore durante la lettura della risposta:", err)
+		return
+	}
+
+	fmt.Println(string(body))
+
+}
+
+func GetRequest(url string) {
+
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Errore durante l'esecuzione della richiesta:", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Errore durante la lettura della risposta:", err)
+		return
+	}
+
+	fmt.Println(string(body))
+}
+
+func GetDelete(url string) {
+
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Errore durante l'esecuzione della richiesta:", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Errore durante la lettura della risposta:", err)
+		return
+	}
+
+	fmt.Println(string(body))
+}
